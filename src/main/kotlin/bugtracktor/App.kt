9e3531d0a3065cd046d37892f0a751b4524636a9
Alex Isaienko <sonerik@mail.ua>
2017-06-05 10:41:10 +0300

    Improved Jackson config.

--- src/main/kotlin/bugtracktor/App.kt
+++ src/main/kotlin/bugtracktor/App.kt
@@ -4,7 +4,9 @@ import bugtracktor.models.User
 import bugtracktor.repositories.IssueRepository
 import bugtracktor.repositories.ProjectRepository
 import bugtracktor.repositories.UserRepository
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.Module
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.module.SimpleModule
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer
 import org.bson.types.ObjectId
@@ -12,6 +14,7 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @SpringBootApplication
 open class App {
@@ -27,8 +30,11 @@ open class App {
     }
 
     @Bean
-    fun parameterNamesModule(): Module {
-        return SimpleModule("ObjectIdModule").addSerializer(ObjectId::class.java, ToStringSerializer())
+    fun jacksonBuilder(): Jackson2ObjectMapperBuilder {
+        return Jackson2ObjectMapperBuilder()
+                .propertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .serializerByType(ObjectId::class.java, ToStringSerializer())
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
     }
 }
 
