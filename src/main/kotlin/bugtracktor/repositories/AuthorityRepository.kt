b0e22e18dadbb1fcc71ce98b0e85bda6cb3cedbe
Alex Isaienko <thesonerik@gmail.com>
2017-06-07 14:40:02 +0300

    AuthorityRepository.kt: removed unused import.

--- src/main/kotlin/bugtracktor/repositories/AuthorityRepository.kt
+++ src/main/kotlin/bugtracktor/repositories/AuthorityRepository.kt
@@ -1,7 +1,6 @@
 package bugtracktor.repositories
 
 import bugtracktor.models.Authority
-import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
 
 interface AuthorityRepository : MongoRepository<Authority, String>
